b019fc08adb4af52c2dc42c0b6259e8e4714fb41
Q4erty <akkemer05@gmail.com>
2025-04-12 18:53:20 +0500

    third commit

--- src/main/java/com/practice/chatservice/controller/ChatController.java
+++ src/main/java/com/practice/chatservice/controller/ChatController.java
@@ -1,16 +1,20 @@
 package com.practice.chatservice.controller;
 
 import com.practice.chatservice.model.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class ChatController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public ChatMessage send(ChatMessage message) {
+        log.info("Received message: {} from {}", message.text(), message.from());
         return message;
     }
+
 }
